cfa56b378b616ed30acee2ab115cd12b57ba69e4
MagmaDevil <naver201010@gmail.com>
2025-07-23 21:43:40 +0900

    [2025.7.23] 선형 큐

---
+++ Class6th (Liner Queue)/Source.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#define SIZE 5
+
+using namespace std;
+
+template <typename T>
+class Queue
+{
+private:
+	int rear;
+	int front;
+
+	T container[SIZE];
+public:
+	Queue()
+	{
+		rear = 0;
+		front = 0;
+
+		for (int i = 0; i < 5; i++)
+		{
+			container[i] = NULL;
+		}
+	}
+
+	void push(T data)
+	{
+		if (rear >= SIZE)
+		{
+			cout << " Liner Queue is overflow!" << endl;
+		}
+
+		else
+		{
+			container[rear++] = data;
+		}
+	}
+
+	void pop()
+	{
+		if (empty())
+		{
+			cout << " Liner Queue is empty!" << endl;
+		}
+
+		else
+		{
+			container[front++] = NULL;
+		}
+	}
+
+	const int& size()
+	{
+		return rear - front;
+	}
+
+	const bool& empty()
+	{
+		return front == rear;
+	}
+
+	const T& peek()
+	{
+		if (empty())
+		{
+			exit(1);
+		}
+
+		else
+		{
+			return container[front];
+		}
+	}
+};
+
+int main()
+{
+	Queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+	queue.push(40);
+	queue.push(50);
+
+	while (queue.empty() == false)
+	{
+		cout << queue.peek() << endl;
+
+		queue.pop();
+	}
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,36 +1,59 @@
 ﻿#include <iostream>
+#define SIZE 4
 
 using namespace std;
 
 template <typename T>
-class Stack
+class Queue
 {
 private:
-	int count;
-	int highset;
-	int capacity;
-	T * container;
+	int rear;
+	int front;
 
+	T container[SIZE];
 public:
-	Stack()
+	Queue()
 	{
-		count = 0;
-		highset = -1;
-		capacity = 0;
-		container = nullptr;
+		rear = SIZE - 1;
+		front = SIZE - 1;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			container[i] = NULL;
+		}
 	}
 
-	~Stack()
+	void push(T data)
 	{
-		if (container != nullptr)
+		if(rear + 1 & SIZE - 1)
 		{
-			delete[] container;
-		}		
+			cout << "Queue is overflow!" << endl;
+		}
+
+		else
+		{
+			if(rear + 1 >= SIZE -1)
+			{
+				rear = 0;
+				container[++rear] = data;
+			}
+
+			else
+			{
+				container[++rear] = data;
+			}
+		}
 	}
 };
 
 int main()
 {
+	Queue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+	queue.push(40);
 
 	return 0;
 }
