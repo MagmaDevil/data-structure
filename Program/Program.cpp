17521af01bca07200db2d9646da1d65cc6fd8cea
MagmaDevil <207029261+MagmaDevil@users.noreply.github.com>
2025-08-05 21:43:46 +0900

    [2025.8.5] 이진 탐색 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -9,8 +9,13 @@ private:
 	struct Node
 	{
 		T data;
-		Node* left;
-		Node* right;
+		Node* left = nullptr;
+		Node* right = nullptr;
+
+		Node(T data)
+		{
+			this->data = data;
+		}
 	};
 
 	Node* root;
@@ -22,24 +27,139 @@ public:
 
 	void insert(T data)
 	{
+		Node * newNode = new Node(data);
+		
 		if(root == nullptr)
 		{
-			root->data = data;
+			root = newNode;
 		}
 
+		else
+		{
+			Node* currentNode = root;
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data > data)
+				{
+					delete newNode;
+
+					return;
+				}
+
+				else if (currentNode->data > data)
+				{
+					if(currentNode->left == nullptr)
+					{
+						currentNode->left = newNode;
+
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+
+				else
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = newNode;
+
+						break;
+					}
+
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+			}
+		}
+	}
+
+	void release(Node * root)
+	{
+		if (root != nullptr)
+		{
+			release(root->left);
+			release(root->right);
+
+			delete root;
+		}
+	}
+
+	void erase(T data)
+	{
 		Node* currentNode = root;
-		Node * newNode = new Node;
-		newNode = data;
+		Node* parentNode = nullptr;
 
-		if (data < root->data)
+		while(currentNode != nullptr && currentNode->data != data)
 		{
-			currentNode->left = newNode;
+			if(currentNode->data > data)
+			{
+				currentNode = currentNode->left;
+			}
+
+			else
+			{
+				currentNode = currentNode->right;
+			}
 		}
 
-		else if(data > root->data)
+		if (currentNode == nullptr)
 		{
-			currentNode->right = newNode;
+			cout << "삭제할 대상이 없습니다." << endl;
 		}
+
+		if (currentNode->left == nullptr && currentNode->right == nullptr)
+		{
+			if (currentNode == root)
+			{
+				delete root;
+
+				root = nullptr;
+			}
+
+			else
+			{
+				if (parentNode->left == currentNode)
+				{
+					parentNode->left = nullptr;
+				}
+					
+				else
+				{
+					parentNode->right = nullptr;
+				}
+
+				delete currentNode;
+			}
+		}
+
+		else if (currentNode->left == nullptr || currentNode->right == nullptr)
+		{
+			Node* child = (currentNode->left != nullptr) ? currentNode->left : currentNode->right;
+
+			if (currentNode == root)
+			{
+				root = child;
+			}
+			else
+			{
+				if (parentNode->left == currentNode)
+					parentNode->left = child;
+				else
+					parentNode->right = child;
+			}
+
+			delete currentNode;
+		}
+	}
+
+	~Set()
+	{
+		release(root);
 	}
 };
 
@@ -48,8 +168,10 @@ int main()
 	Set<int> set;
 
 	set.insert(10);
-	set.insert(5);
-	set.insert(15);
+	set.insert(6);
+	set.insert(20);
+	set.insert(3);
+	set.insert(3);
 
 	return 0;
 }
